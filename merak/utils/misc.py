c4a222bc68619a2c3dc2cb1c1bd821026ea3d63b
(David) Siu-Kei Muk <david.muk@protonmail.com>
2021-05-02 08:31:33 +0800

    feat: Add lazy property util

--- merak/utils/misc.py
+++ merak/utils/misc.py
@@ -1,3 +1,13 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+
+
+class LazyProperty(object):
+  def __init__(self, function):
+    self.function = function
+    self.name = function.__name__
+
+  def __get__(self, instance, owner):
+    instance.__dict__[self.name] = self.function(instance)
+    return instance.__dict__[self.name]
