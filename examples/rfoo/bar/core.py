c290ac6394a401b51d72e0b3f306521e5ad9fde7
David Siu-Kei Muk <20377823+dave-msk@users.noreply.github.com>
2022-08-27 14:21:36 +0800

    feat: Support relative imports
    - Add test package that uses relative imports

---
+++ examples/rfoo/bar/core.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from ..constant import CONSTANT
+
+
+def print_constant():
+  print(CONSTANT)
